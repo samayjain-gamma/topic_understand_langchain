367fd723a28a162ad7526cb9c9d0dcfc917edd3e
Samay Jain <samay.jain@gammaedge.io>
2026-02-24 13:38:44 +0530

    project structure added

---
+++ app/config.py
@@ -0,0 +1,36 @@
+import os
+from langchain_community.chat_models import ChatOllama
+from app.utils.exception import CustomException 
+from app.utils.logger import logger
+
+
+def get_llm():
+    """
+        Initialize and return the instance of Ollama LLm model
+        Model -> tinyllama        
+    """
+
+    try:
+        model_name = os.getenv("OLLAMA_MODEL", "tinyllama")
+
+        llm = ChatOllama(
+            model = model_name,
+            temperature =  0.2,
+            num_predict= 512    # Limit response length      
+        )
+
+        logger.info(f"LLM initialized successfully with modekl {model_name}")
+
+
+
+        return llm
+    
+    except Exception as e:
+        logger.error("Failed to initialize LLM")
+        raise CustomException(e)
+    
+
+if __name__ == "__main__":
+    llm = get_llm()
+    response = llm.invoke("HEllo") 
+    print(response.content)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from topic-understanding!")
+
+
+if __name__ == "__main__":
+    main()
